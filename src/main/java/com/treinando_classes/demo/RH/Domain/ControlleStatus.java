9a30fa207fdd19a4ececce9984c6186f78f3fa7d
dev pedro <pedro751ok@gmail.com>
2026-08-29 09:54:12 -0300

    crição do rbac em RH

--- src/main/java/com/treinando_classes/demo/RH/Domain/ControlleStatus.java
+++ src/main/java/com/treinando_classes/demo/RH/Domain/ControlleStatus.java
@@ -1,5 +1,47 @@
 package com.treinando_classes.demo.RH.Domain;
+import com.treinando_classes.demo.RH.regras_Enums.RegrasDeEnums;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
 public class ControlleStatus {
-    public
+    public static final Map<RegrasDeEnums.CargoRh, Set<RegrasDeEnums.Permissoes>> PERMISSOES =
+            new EnumMap<>(RegrasDeEnums.CargoRh.class);
+    static{
+    PERMISSOES.put(RegrasDeEnums.CargoRh.RH_ASSISTENTE, EnumSet.of(
+            RegrasDeEnums.Permissoes.CRIAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.CANCELAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.EXIBIR_SOLICITACAO
+    ));
+
+    PERMISSOES.put(RegrasDeEnums.CargoRh.RH_ANALISTA, EnumSet.of(
+            RegrasDeEnums.Permissoes.CRIAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.CANCELAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.EXIBIR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.ACEITAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.REJEITAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.GERENCIAR_AFASTAMENTO,
+            RegrasDeEnums.Permissoes.GERENCIAR_CID
+    ));
+    PERMISSOES.put(RegrasDeEnums.CargoRh.RH_GERENTE, EnumSet.of(
+
+            RegrasDeEnums.Permissoes.CRIAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.CANCELAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.EXIBIR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.ACEITAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.REJEITAR_SOLICITACAO,
+            RegrasDeEnums.Permissoes.GERENCIAR_AFASTAMENTO,
+            RegrasDeEnums.Permissoes.GERENCIAR_CID,
+            RegrasDeEnums.Permissoes.GERENCIAR_SOLICITACOES,
+            RegrasDeEnums.Permissoes.GERENCIAR_EMPRESA,
+            RegrasDeEnums.Permissoes.HISTORICO_SOLICITACOES,
+            RegrasDeEnums.Permissoes.LISTAR_SOLICITACOES_PENDENTES,
+            RegrasDeEnums.Permissoes.GERENCIAR_FUNCIONARIO
+    ));
+
+        }
+    public static Boolean controlledeacesso(RegrasDeEnums.CargoRh role, RegrasDeEnums.Permissoes permissao){
+        return PERMISSOES.getOrDefault(role, Set.of()).contains(permissao);
+    }
 }
